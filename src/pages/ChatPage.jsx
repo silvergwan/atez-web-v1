e05781e7655df998fb92ca71ecb71dc308632a2d
EUNGWANCHOI <silvergwan@gmail.com>
2026-04-20 10:48:30 +0900

    update: tailwind로 마이그레이션

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,12 +1,20 @@
 import ChatWindow from "../components/ChatWindow";
-import "../index.css";
-import kmj from "../assets/kmj.png";
 
 export default function ChatPage() {
   return (
-    <div className="chat-page">
-      <div className="chat-container">
-        <div className="text-blue-500">유민아</div>
+    <div className="min-h-screen w-full flex justify-center bg-black">
+      <div
+        className="w-120 h-screen flex flex-col"
+        style={{ backgroundColor: "#151516" }}
+      >
+        {/* 헤더 */}
+        <header className="h-13 flex items-center px-4 shrink-0 border-b border-white/10">
+          <span className="ml-3 text-white font-semibold text-[16px]">
+            유민아
+          </span>
+        </header>
+
+        {/* 메시지 영역 - ChatWindow가 채울 예정 */}
         <ChatWindow />
       </div>
     </div>

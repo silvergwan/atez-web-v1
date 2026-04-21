5ce372824fcc46f6fb4a344d9913ac38ff5413ac
EUNGWANCHOI <silvergwan@gmail.com>
2026-04-21 10:39:54 +0900

    update: messageBubble 패딩 수정(12, 16px -> 8, 12px)

--- src/components/MessageBubble.jsx
+++ src/components/MessageBubble.jsx
@@ -30,7 +30,7 @@ export default function MessageBubble({ role, text, isError }) {
     <div className={`flex ${isUser ? "justify-end" : "justify-start"}`}>
       <div
         className={`
-          max-w-[75%] px-4 py-3 text-[14px] leading-relaxed whitespace-pre-line
+          max-w-[75%] px-3 py-2 text-[14px] leading-relaxed whitespace-pre-line
           ${
             isUser
               ? "bg-[#644DD6] rounded-[16px_0px_16px_16px]"
